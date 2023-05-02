4f2ef14da73b9e1ff985686ccca24e3a86bd4a1e
DefectingCat <i@defect.ink>
2023-05-02 18:21:40 +0800

    Add illgeal headers for test

--- tests/illegal_header_test.rs
+++ tests/illegal_header_test.rs
@@ -10,12 +10,11 @@ async fn illegal_headers() {
     let mut stream = TcpStream::connect("localhost:3000").await.unwrap();
     let (read, mut write) = stream.split();
     let headers = r#"GET / HTTP/1.1
-Host: developer.mozilla.org
-User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:50.0) Gecko/20100101 Firefox/50.0
 Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
 Accept-Language: en-US,en;q=0.5
 Accept-Encoding: gzip, deflate, br
 Cache-Control: max-age=0
+abcd
 
 "#;
     println!("{headers}");
