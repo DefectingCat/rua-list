c0a7a1661fe66e16177e4dd4ff638ba0c7171078
xfy <i@rua.plus>
2025-04-01 11:01:19 +0800

    chore(api): change backend address

--- src/consts.rs
+++ src/consts.rs
@@ -1,2 +1,4 @@
 // Target address
-pub static MSG_URL: &str = "https://swdx.kdah.cn:8011";
+// pub static MSG_URL: &str = "https://swdx.kdah.cn:8011";
+
+pub static MSG_URL: &str = "http://58.242.187.12:8011";
